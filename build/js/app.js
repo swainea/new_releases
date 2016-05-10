4f1c0c0527ac2dc588f2aec35621b6e46cfbf2c7
Liz Swain <swainea@gmail.com>
2016-05-10 13:59:33 -0400

    able to change window.location to spotify authorization

--- build/js/app.js
+++ build/js/app.js
@@ -42,7 +42,24 @@
 
   function LoginController($stateParams, $state){
     console.log('in login controller');
+
+    this.oAuth = function oAuth (){
+      console.log("inside oAuth");
+      var client_id = '76448191f52d4674a641b52162d19c85';
+      var redirect_uri = 'inserthere';
+
+      var url = 'https://accounts.spotify.com/authorize';
+      url += '?response_type=token';
+      url += '&client_id=' + encodeURIComponent(client_id);
+
+
+      window.location = url;
+    };
+
+
   }
+
+
 }());
 
 //# sourceMappingURL=app.js.map
