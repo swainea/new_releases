73c43269988632d54d1b274d12e68bd3a990cb41
Liz Swain <swainea@gmail.com>
2016-05-16 17:10:18 -0400

    rewrote tests to target specific functionality

--- test/specs/spotify.service.js
+++ test/specs/spotify.service.js
@@ -13,60 +13,80 @@
       $httpBackend = _$httpBackend_;
 
       $httpBackend
-        .whenGET('https://api.spotify.com/v1/browse/new-releases?offset=0&limit=50')
-        .respond(
-          {
-            albums: {
-              items: [{
-                  external_urls: {
-                    spotify: 'https://test.testalbum/test'
-                  },
-                  href: 'https://test.test.com/test',
-                  id: '123',
-                  images: [
-                    { height: 640, url: 'https://test.test.com/testimage', width: 640 },
-                    { height: 300, url: 'https://test.test.com/testimage', width: 300 },
-                    { height: 64, url: 'https://test.test.com/testimage', width: 64 }
-                  ],
-                  name: 'NewAlbumTestName',
-                  type: 'album'
-                },
-                {
-                  external_urls: {
-                    spotify: 'https://test.testalbum/test'
-                  },
-                  href: 'https://test.test.com/test',
-                  id: '425',
-                  images: [
-                    { height: 640, url: 'https://test.test.com/testimage2', width: 640 },
-                    { height: 300, url: 'https://test.test.com/testimage2', width: 300 },
-                    { height: 64, url: 'https://test.test.com/testimage2', width: 64 }
-                  ],
-                  name: 'NewAlbumTestName2',
-                  type: 'album'
-                }
-              ]
+      .whenGET('https://api.spotify.com/v1/browse/new-releases?offset=0&limit=50')
+      .respond(
+        {
+          albums: {
+            items: [{
+              external_urls: {
+                spotify: 'https://test.testalbum/test'
+              },
+              href: 'https://test.test.com/test',
+              id: '123',
+              images: [
+                { height: 640, url: 'https://test.test.com/testimage', width: 640 },
+                { height: 300, url: 'https://test.test.com/testimage', width: 300 },
+                { height: 64, url: 'https://test.test.com/testimage', width: 64 }
+              ],
+              name: 'NewAlbumTestName',
+              type: 'album'
+            },
+            {
+              external_urls: {
+                spotify: 'https://test.testalbum/test'
+              },
+              href: 'https://test.test.com/test',
+              id: '425',
+              images: [
+                { height: 640, url: 'https://test.test.com/testimage2', width: 640 },
+                { height: 300, url: 'https://test.test.com/testimage2', width: 300 },
+                { height: 64, url: 'https://test.test.com/testimage2', width: 64 }
+              ],
+              name: 'NewAlbumTestName2',
+              type: 'album'
             }
-          }
-      );
-    }));
+          ]
+        }
+      }
+    );
+  }));
 
-    test('getting new releases', function(doneCallback){
-      var token = 123456;
-      SpotifyService.getNewReleases(token)
-        .then(function(items){
-          assert.strictEqual(1 === 1, true, 'sanity test');
-          assert.isArray(items, "items returns an array");
-          assert.ok(items[0].external_urls, "items includes an external_urls object");
-          assert.strictEqual(items.length, 2, 'correct number of results'); 
-          doneCallback();
-        })
-        .catch(function(){
-          assert.ok(false, 'should not fail promise');
-          doneCallback();
+  test('service methods exist', function(){
+    assert.isFunction(SpotifyService.getNewReleases, "getNewReleases method exisits");
+  });
+
+  test('getNewReleases functions as expected', function(doneCallback){
+    var token = 123456;
+    var returnValue = SpotifyService.getNewReleases(token);
+    assert.ok(returnValue, 'returnValue exisits');
+    assert.isFunction(returnValue.then, 'returnValue returns a promise');
+
+    returnValue.then(function(items){
+      assert.isArray(items, "items returns an array");
+      doneCallback();
+    })
+    .catch(function(){
+      assert.ok(false, 'should not fail promise');
+      doneCallback();
     });
     $httpBackend.flush();
   });
 
+  test('invalid access token', function(){
+    var token = "null";
+    var returnValue = SpotifyService.getNewReleases(token);
+
+    returnValue.then(function(){
+      assert.ok(false, 'should not send request');
+      doneCallback();
+    })
+    .catch(function(){
+      // here i want to assert that we get into the catch if invalid token
+      doneCallback();
+    });
+    $httpBackend.flush();
   });
+
+
+});
 }());
