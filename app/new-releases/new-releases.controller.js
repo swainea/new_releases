ee4aa0a6dc3acf36590c4e4516237e7107c608ca
Liz Swain <swainea@gmail.com>
2016-05-17 14:57:15 -0400

    added iniit function on NewReleasesController

--- app/new-releases/new-releases.controller.js
+++ app/new-releases/new-releases.controller.js
@@ -5,31 +5,41 @@
     .module('app')
     .controller('NewReleasesController', NewReleasesController);
 
-  NewReleasesController.$inject = ['$state', 'SpotifyService'];
+  NewReleasesController.$inject = ['$state', '$q', 'SpotifyService'];
 
-  function NewReleasesController($state, SpotifyService){
+  function NewReleasesController($state, $q, SpotifyService){
     var that = this;
     this.albumData = [];
     this.errorMessage = "";
 
-    if (!localStorage.getItem("access_token")){
+    this.init = function init(){
+      if (!localStorage.getItem("access_token")){
+
         $state.go('home');
 
-    } else {
-    
-    SpotifyService.getNewReleases(localStorage.getItem("access_token"))
-      .then(function ( response ){
-        console.log("response: ", response);
-        that.albumData =  response;
-        console.log("that.albumData: ", that.albumData);
-        console.log("images: ", that.albumData[0].images[0].url);
-        console.log("link to spotify: ", that.albumData[0].external_urls.spotify);
-      })
-      .catch(function onError ( reponse ){
-        console.log ('error response: ', response );
-        that.errorMessage = "Unable to process request. Please try again.";
-      });
-    }
+        var deferred = $q.defer();
+        deferred.reject('You must authorize with Spotify to continue');
+        console.log('Reject: ', deferred.promise);
+        return deferred.promise;
+
+      } else {
+
+      return SpotifyService.getNewReleases(localStorage.getItem("access_token"))
+        .then(function ( response ){
+          // console.log("response: ", response);
+          that.albumData =  response;
+
+          // console.log("that.albumData: ", that.albumData);
+          // console.log("images: ", that.albumData[0].images[0].url);
+          // console.log("link to spotify: ", that.albumData[0].external_urls.spotify);
+        })
+        .catch(function onError ( reponse ){
+          console.log ('error response: ', response );
+          that.errorMessage = "Unable to process request. Please try again.";
+        });
+      }
+    };
+
 
   }
 
