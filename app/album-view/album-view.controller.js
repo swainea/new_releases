e50fbca56905d68a6551dca1a5303c928241652c
Liz Swain <swainea@gmail.com>
2016-05-18 14:50:42 -0400

    added controller that renders data from Spotify Service

--- app/album-view/album-view.controller.js
+++ app/album-view/album-view.controller.js
@@ -5,9 +5,28 @@
     .module('app')
     .controller('AlbumViewController', AlbumViewController);
 
-  AlbumViewController.$inject = ['$stateParams'];
+  AlbumViewController.$inject = ['$state', '$stateParams', 'SpotifyService'];
 
-  function AlbumViewController($stateParams){
-    console.log("Params: ", $stateParams.album);
+  function AlbumViewController($state, $stateParams, SpotifyService){
+    var that = this;
+    this.album = $stateParams.album;
+    console.log(this.album);
+    this.artistData = [];
+
+    this.init = function init(){
+      if(this.album === null){
+        $state.go('new-releases');
+      } else {
+        SpotifyService.getArtistData( this.album.href )
+          .then(function ( response ){
+            console.log("response: ", response);
+            that.artistData = response;
+          });
+      }
+    };
+
+    this.listen = function listen(){
+       
+    }
   }
 }());
