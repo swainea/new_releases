1e6139f0afa8b0d454a5dc0134dc09ed21717f16
Liz Swain <swainea@gmail.com>
2016-05-24 11:04:27 -0400

    generic error message renders in UI on fail for RA controller

--- app/recommended-artists/recommended-artists.controller.js
+++ app/recommended-artists/recommended-artists.controller.js
@@ -12,19 +12,22 @@
     var that = this;
     this.id = $stateParams.id;
     this.recs = [];
+    this.errorMessage = "";
 
     this.init = function init(){
-
         SpotifyService.getRecommendedArtists( this.id )
-          .then(function (response){
+          .then(function ( response ){
             console.log('recs response: ', response);
             that.recs = response;
+          })
+          .catch(function onError ( response ){
+            that.errorMessage = "Unable to process request. Please try again.";
           });
     };
 
     this.back = function back(){
       $state.go('new-releases');
     };
-    
+
 }
 }());
