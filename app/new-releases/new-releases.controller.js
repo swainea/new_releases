454fcc9584b0febc7aa43d92911bbc55a08b3dbf
Liz Swain <swainea@gmail.com>
2016-05-24 10:40:34 -0400

    error for new releases controller renders in UI and has bold red styling

--- app/new-releases/new-releases.controller.js
+++ app/new-releases/new-releases.controller.js
@@ -14,27 +14,16 @@
 
     this.init = function init(){
       if (!localStorage.getItem("access_token")){
-
-        $state.go('home');
-
-        var deferred = $q.defer();
-        deferred.reject('You must authorize with Spotify to continue');
-        console.log('Reject: ', deferred.promise);
-        return deferred.promise;
+        // I need to pass an error message as a state param into the login
+        $state.go('home', {'errMsg': "Please Login First"} );
 
       } else {
 
       return SpotifyService.getNewReleases(localStorage.getItem("access_token"))
         .then(function ( response ){
-          // console.log("response: ", response);
           that.albumData =  response;
-
-          console.log("that.albumData: ", that.albumData);
-          // console.log("images: ", that.albumData[0].images[0].url);
-          // console.log("link to spotify: ", that.albumData[0].external_urls.spotify);
         })
         .catch(function onError ( reponse ){
-          console.log ('error response: ', response );
           that.errorMessage = "Unable to process request. Please try again.";
         });
       }
